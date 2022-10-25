2a9af8c36bc63e5b339ce301633cf450636c4659
Jeni Williams <Jeni.Williams@walmart.com>
2022-10-25 13:02:54 -0700

    Adopt Me Project - complete through Unit 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import { render } from "react-dom";
-import SearchParams from "../SearchParams";
-//import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me Pet Company</h1>
       <SearchParams />
-      {/* <Pet
-        name="Lucky"
-        animal="Favorite Dog"
-        breed="Black Lab Special Edition"
-      />
-      <Pet
-        name="Dusty"
-        animal="Another Dog"
-        breed="German Shepherd Plus Edition"
-      />
-      <Pet name="Eliot" animal="Secret Cat" breed="Garfield in Real Life" /> */}
     </div>
   );
 };
